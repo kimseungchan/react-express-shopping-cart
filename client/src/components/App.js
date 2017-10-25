48a4bbfb60142ef2d66c685b177cd11f4cad690d
gogolearn <jay.jongcheol.park@gmail.com>
2017-10-25 13:18:54 -0400

    Organize file structure

---
+++ client/src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Main from './Main'
+
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Main} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.querySelector('#root'));
